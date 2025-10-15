0efb103ea55e23409bf500ae0f3642a0764fa316
Bukov Kirill Sergeevich IP-514 <kirillkabukov228@gmail.com>
2025-10-15 20:45:03 +0700

    ISPRAVLENO PRIVETSTVIE

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char** argv){
 	char* str =(char*)malloc(seziof(char)* SIZE;
 	printf("ENTER YOUR NAME:\n");
 	scanf("%s", str);
-	printf("HEllo, %s", str);
+	printf("HEllo");
 	return 0;
 }
