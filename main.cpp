f7b7d5e75dc3ba9254963979b642da74f9825690
Bukov Kirill Sergeevich IP-514 <kirillkabukov228@gmail.com>
2025-10-15 20:11:26 +0700

    initial committ

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 1024
+int main(int argc, char** argv){
+	char* str =(char*)malloc(seziof(char)* SIZE;
+	printf("ENTER YOUR NAME:\n");
+	scanf("%s", str);
+	printf("HEllo, %s", str);
+	return 0;
+}
